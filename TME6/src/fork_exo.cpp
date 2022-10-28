10a9c0ee8ab4bd508022334b022aec764617be67
HAMMACHI <28604304@ppti-24-303-13.ufr-info-p6.jussieu.fr>
2022-10-28 12:24:10 +0200

    Debut TME6

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main () {
 	const int N = 3;
 	std::cout << "main pid=" << getpid() << std::endl;
-
+	int nbFils = 0;
 	for (int i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
+		nbFils = 0;
 		std::cout << " i:j " << i << ":" << j << std::endl;
 		for (int k=1; k<=i && j==N ; k++) {
 			if ( fork() == 0) {
+				nbFils = 0;
 				j=0;
 				std::cout << " k:j " << k << ":" << j << std::endl;
 			}
+			else{
+				nbFils++;
+			}
 		}
+	}
+	nbFils++;
+	std::cout << "nbFils : " << nbFils << std::endl;
+	for(int i=0; i < nbFils; i++){
+		wait(nullptr);
 	}
 	return 0;
 }
