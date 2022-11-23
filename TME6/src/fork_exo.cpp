302f347b56fccc7583b6fb5d9ae5b792c2574b20
Nitrome <https://Nitrome@github.com>
2022-11-23 21:37:01 +0100

    Correction nbFils

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main () {
 	const int N = 3;
 	std::cout << "main pid=" << getpid() << std::endl;
-
-	for (int i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
+	int nbFils = 0;
+	int i;
+	for (i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
+		nbFils = 0;
 		std::cout << " i:j " << i << ":" << j << std::endl;
 		for (int k=1; k<=i && j==N ; k++) {
 			if ( fork() == 0) {
+				nbFils = 0;
 				j=0;
 				std::cout << " k:j " << k << ":" << j << std::endl;
 			}
+			else{
+				nbFils++;
+			}
 		}
+	}
+	if(!(i >= N)) //Le dernier fils ne fork pas a cause de la condition de fin
+		nbFils++;
+	std::cout << "nbFils : " << nbFils << std::endl;
+	for(int i=0; i < nbFils; i++){
+		wait(nullptr);
 	}
 	return 0;
 }
