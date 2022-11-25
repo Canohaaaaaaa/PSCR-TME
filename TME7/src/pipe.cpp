52a6466672de380c292ead6f29243aa637f5a7d3
HAMMACHI <28604304@ppti-24-303-14.ufr-info-p6.jussieu.fr>
2022-11-25 12:27:01 +0100

    Pipe commande fin

--- TME7/src/pipe.cpp
+++ TME7/src/pipe.cpp
@@ -6,13 +6,15 @@
 using namespace std;
 int main(int argc, char* argv[]){
 	if(argc < 3){
+		perror("mauvais usage");
 		return 1;
 	}
-	int tube[2];
 	size_t i;
+	//** Segmentation des commandes **//
 	for(i = 1; string(argv[i]) != "|"; i++){
 		/*nop*/
 	}
+	argv[i] = NULL;
 	cout << "Commande 1 : " << endl;
 	for(size_t j = 1; j < i; j++){
 		cout << argv[j] << " ";
@@ -23,5 +25,39 @@ int main(int argc, char* argv[]){
 		cout << argv[k] << " ";
 	}
 	cout << endl;
+
+	//** Pipe pere-fils **//
+	int tube[2];
+	pid_t pid_fils = fork();
+	if (pid_fils == -1){
+		return 1;
+	}
+	char** arg_commande;
+	if(pipe(tube) == -1){
+		perror("pipe");
+		return 1;
+	}
+	if (pid_fils == 0) { //Fils
+		arg_commande = &argv[i+1];
+		//cout << arg_commande[0] << endl;
+		dup2(tube[1],STDOUT_FILENO);
+		close (tube[1]);
+		close (tube[0]);
+		if(execv(argv[i+1], &argv[i+1]) == -1){
+			perror("execv");
+			return 1;
+		}
+	}
+	else { //Pere
+		arg_commande = &argv[2];
+		//cout << arg_commande[0] << endl;
+		dup2(tube[0],STDIN_FILENO);
+		close (tube[0]);
+		close (tube[1]);
+		if (execv(argv[1], &argv[1]) == -1) {
+			perror ("execv");
+			return 1;
+		}
+	}
 	return 0;
 }
