42186d7caa4b19ded09353120f02fe78e07b9041
Nitrome <https://Nitrome@github.com>
2022-12-02 00:04:48 +0100

    Semaphores

--- TME7/src/Stack.h
+++ TME7/src/Stack.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <cstring> // size_t,memset
-
+#include <semaphore.h>
+#include <fcntl.h>
 namespace pr {
 
 #define STACKSIZE 100
@@ -10,18 +11,37 @@ template<typename T>
 class Stack {
 	T tab [STACKSIZE];
 	size_t sz;
+	sem_t* semaphore_conso;
+	sem_t* semaphore_prod;
 public :
-	Stack () : sz(0) { memset(tab,0,sizeof tab) ;}
+	Stack () : sz(0) { 
+		memset(tab,0,sizeof tab);
+		semaphore_conso = sem_open("/stacksemconso", O_CREAT | O_RDWR, 0600, 1);
+		semaphore_prod = sem_open("/stacksemprod", O_CREAT | O_RDWR, 0600, 1);
+	}
 
 	T pop () {
-		// bloquer si vide
-		T toret = tab[--sz];
-		return toret;
+		if(!sz){
+			sem_wait(semaphore_conso);
+			T toret = tab[--sz];
+			sem_post(semaphore_prod);
+			return toret;
+		}
+		else{
+			T toret = tab[--sz];
+			return toret;
+		}
 	}
 
 	void push(T elt) {
-		//bloquer si plein
-		tab[sz++] = elt;
+		if(sz == STACKSIZE){
+			sem_wait(semaphore_prod);
+			tab[sz++] = elt;
+			sem_post(semaphore_conso);
+		}
+		else{
+			tab[sz++] = elt;
+		}
 	}
 };
 
